3ef9b76dcef49dc139e8c9e71d572799b3873d2d
udaykumarsama <113637491+udaykumarsama@users.noreply.github.com>
2022-11-17 13:36:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,82 @@
-const http = require("http");
-const fs = require("fs");
-
-let homeContent = "";
-let projectContent = "";
-let registrationContent="";
-
-fs.readFile("home.html", (err, home) => {
-  if (err) {
-    throw err;
+const todoList = () => {
+  all = []
+  const add = (todoItem) => {
+    all.push(todoItem)
   }
-  homeContent = home;
-});
-
-fs.readFile("project.html", (err, project) => {
-  if (err) {
-    throw err;
+  const markAsComplete = (index) => {
+    all[index].completed = true
   }
-  projectContent = project;
-});
 
-fs.readFile("registration.html",(err,reg) => {
-  if (err){
-    throw err;
+  const overdue = () => {
+      let overdue_list = all.filter((item) => item.dueDate < today);
+      return overdue_list;
   }
-  registrationContent=reg;
-});
 
-let args=require("minimist")(process.argv.slice(2));
+  const dueToday = () => {
+      let dueToday_list = all.filter((item) => item.dueDate === today);
+      return dueToday_list;
+  }
 
-http.createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-        case "/project":
-          response.write(projectContent);
-          response.end();
-          break;
-        case "/registration":
-          response.write(registrationContent);
-          response.end();
-          break;
+  const dueLater = () => {
+      let dueLater_list = all.filter((item) => item.dueDate > today);
+      return dueLater_list;
+    
+  }
 
-        default:
-          response.write(homeContent);
-          response.end();
-          break;
-    }
-  })
-  .listen(args["port"]);
+  const toDisplayableList = (list) => {
+      let display_list = list.map((item) => {
+          let completionStatus = item.completed ? "[x]": "[ ]";
+          let displayedDate = item.dueDate === today ? "" : item.dueDate;
+          return `${completionStatus} ${item.title} ${displayedDate}`.trim();
+        });
+        let output_string = display_list.join("\n");
+        return output_string;
+  }
+
+  return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
+}
+
+// ####################################### #
+// DO NOT CHANGE ANYTHING BELOW THIS LINE. #
+// ####################################### #
+
+const todos = todoList();
+
+const formattedDate = d => {
+  return d.toISOString().split("T")[0]
+}
+
+var dateToday = new Date()
+const today = formattedDate(dateToday)
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+)
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+)
+
+todos.add({ title: 'Submit assignment', dueDate: yesterday, completed: false })
+todos.add({ title: 'Pay rent', dueDate: today, completed: true })
+todos.add({ title: 'Service Vehicle', dueDate: today, completed: false })
+todos.add({ title: 'File taxes', dueDate: tomorrow, completed: false })
+todos.add({ title: 'Pay electric bill', dueDate: tomorrow, completed: false })
+
+console.log("My Todo-list\n\n")
+
+console.log("Overdue")
+var overdues = todos.overdue()
+var formattedOverdues = todos.toDisplayableList(overdues)
+console.log(formattedOverdues)
+console.log("\n\n")
+
+console.log("Due Today")
+let itemsDueToday = todos.dueToday()
+let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday)
+console.log(formattedItemsDueToday)
+console.log("\n\n")
+
+console.log("Due Later")
+let itemsDueLater = todos.dueLater()
+let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater)
+console.log(formattedItemsDueLater)
+console.log("\n\n")
